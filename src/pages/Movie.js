0430258c4e713fe1f1118197c7d211ab9e8bb0ff
João Granado <joao.granado@farfetch.com>
2019-02-18 10:02:29 +0000

    Add fifth lesson bootstrap

---
+++ src/pages/Movie.js
@@ -0,0 +1,19 @@
+/**
+ * Module dependencies.
+ */
+
+import './Movie.css';
+import Container from '../components/Container';
+import React from 'react';
+
+/**
+ * `Movie` component.
+ */
+
+export default class Movie extends React.Component {
+  render() {
+    return (
+      <Container/>
+    );
+  }
+}
